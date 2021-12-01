246eb7d49a57ca2e2494c6a453c9c90b34cf3d76
Arrezz <hansarvid96@hotmail.se>
2021-12-01 13:59:45 +0100

    Initial commit

---
+++ 01/main.py
@@ -0,0 +1,14 @@
+def solver(measurements):
+    increase = 0
+
+    for i in range(1, len(measurements)):
+        if measurements[i] > measurements[i-1]:
+            increase += 1
+
+    print(increase)
+
+
+if __name__ == '__main__':
+    with open("./inputs/day1.txt") as file:
+        data = [int(line.strip()) for line in file]
+    solver(data)
