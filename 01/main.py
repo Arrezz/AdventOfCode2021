ff1a905b3c63dff6c50a8a0df18ffe03015c92e1
Arrezz <hansarvid96@hotmail.se>
2021-12-01 17:30:36 +0100

    Solved problem 2 now!

--- 01/main.py
+++ 01/main.py
@@ -1,10 +1,21 @@
-#Only problem 1 for now!
+# Only problem 1 for now!
 
-def solver(measurements):
+def solverProblem1(measurements):
     increase = 0
 
     for i in range(1, len(measurements)):
-        if measurements[i] > measurements[i-1]:
+        if measurements[i] > measurements[i - 1]:
+            increase += 1
+
+    print(increase)
+
+
+def solverProblem2(measurements):
+    increase = 0
+
+    for i in range(2, len(measurements) - 1):
+        if (measurements[i - 2] + measurements[i - 1] + measurements[i]) < (
+                measurements[i - 1] + measurements[i] + measurements[i + 1]):
             increase += 1
 
     print(increase)
@@ -13,4 +24,5 @@ def solver(measurements):
 if __name__ == '__main__':
     with open("./inputs/day1.txt") as file:
         data = [int(line.strip()) for line in file]
-    solver(data)
+    solverProblem1(data)
+    solverProblem2(data)
